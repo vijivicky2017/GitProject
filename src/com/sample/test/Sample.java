e187671c6f4c6a14f68e2a80d070ab97abeb1b0a
vijivicky2017 <vijilavigneshwar25@gmail.com>
2021-07-01 23:40:34 +0530

    Samples Push

---
+++ src/com/sample/test/Sample.java
@@ -0,0 +1,19 @@
+package com.sample.test;
+
+public class Sample {
+	
+	public void employeeId() {
+		System.out.println("123");
+	}
+	public void employeeName() {
+		System.out.println("vijila");
+	}
+
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.employeeId();
+		s.employeeName();
+
+	}
+
+}
