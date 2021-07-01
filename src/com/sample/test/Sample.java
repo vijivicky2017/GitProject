97f8bbc68922fa5cba94b5de5660917fc4ed32a5
vijivicky2017 <vijilavigneshwar25@gmail.com>
2021-07-02 00:00:16 +0530

    employee details

--- src/com/sample/test/Sample.java
+++ src/com/sample/test/Sample.java
@@ -8,11 +8,14 @@ public class Sample {
 	public void employeeName() {
 		System.out.println("vijila");
 	}
-
+	public void employeeDetail() {
+		System.out.println("vijila123");
+	}
 	public static void main(String[] args) {
 		Sample s=new Sample();
 		s.employeeId();
 		s.employeeName();
+		s.employeeDetail();
 
 	}
 
